9abc637e2400f061c14fd22ae1c6a1639be2641e
Assane Diallo <assane@pando2.com>
2023-10-31 22:18:48 +0100

    Refactor articles#index to display articles in a table format with action links and confirm delete prompt.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
